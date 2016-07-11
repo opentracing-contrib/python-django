1bed389154754ebdc24ce7e0006bb7c063ca850c
= <kacamenzind@gmail.com>
2016-07-11 11:44:35 -0700

    Settings for PyPI publishing

--- setup.py
+++ setup.py
@@ -8,25 +8,21 @@ This extension provides simple integration of OpenTracing in Django applications
 '''
 
 setup(
-    name='Django-OpenTracing',
-    version='0.1.0',
-    # url='http://example.com/flask-sqlite3/',
+    name='django_opentracing',
+    version='0.1',
+    url='https://github.com/kcamenzind/django_opentracing/',
     license='MIT',
     author='Kathy Camenzind',
     author_email='kcamenzind@lightstep.com',
     description='OpenTracing support for Django applications',
     long_description=__doc__,
     packages=['django_opentracing'],
-    zip_safe=False,
-    include_package_data=True,
     platforms='any',
     install_requires=[
         'django',
         'opentracing >= 2.0.0' # enter something about version requirement
     ],
     extras_require=['lightstep']
-    # tests_require=['pytest'],
-    # setup_requires=['pytest-runner'],
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
