ca0a95d3cc25ac631cbdc34cebe5d3f4ecdefd9e
Alan Justino <alan.justino@yahoo.com.br>
2016-11-28 12:10:41 -0200

    Setup.py was broken

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     platforms='any',
     install_requires=[
         'django',
-        'opentracing>=1.1<1.2'
+        'opentracing>=1.1,<1.2'
     ],
     classifiers=[
         'Environment :: Web Environment',
