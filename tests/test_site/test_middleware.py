7099ebe73d115114a24b4c96ba9ae98d70d58a0d
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-08-04 19:15:39 +0200

    Implement OT 2.0 support.

--- tests/test_site/test_middleware.py
+++ tests/test_site/test_middleware.py
@@ -7,16 +7,22 @@ class TestDjangoOpenTracingMiddleware(SimpleTestCase):
         client = Client()
         response = client.get('/untraced/')
         assert response['numspans'] == '1'
-        assert len(settings.OPENTRACING_TRACER._current_spans) == 0
+        assert len(settings.OPENTRACING_TRACER._current_scopes) == 0
 
     def test_middleware_traced(self):
         client = Client()
         response = client.get('/traced/')
         assert response['numspans'] == '1'
-        assert len(settings.OPENTRACING_TRACER._current_spans) == 0
+        assert len(settings.OPENTRACING_TRACER._current_scopes) == 0
 
     def test_middleware_traced_with_attrs(self):
         client = Client()
         response = client.get('/traced_with_attrs/')
         assert response['numspans'] == '1'
-        assert len(settings.OPENTRACING_TRACER._current_spans) == 0
+        assert len(settings.OPENTRACING_TRACER._current_scopes) == 0
+
+    def test_middleware_traced_scope(self):
+        client = Client()
+        response = client.get('/traced_scope/')
+        assert response['active_span'] is not None
+        assert response['request_span'] == response['active_span']
