143d1ddd4cb1d9b1041911c400393ec66e61e5ba
Carlos Alberto Cortez <calberto.cortez@gmail.com>
2018-11-07 13:21:57 +0100

    Enable Django 2 support for the 0.x series.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     packages=['django_opentracing', 'tests'],
     platforms='any',
     install_requires=[
-        'django<2',
+        'django',
         'opentracing>=1.1,<2',
         'six',
     ],
