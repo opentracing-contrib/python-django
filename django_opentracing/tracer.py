75d5c474d1ce5130518f860dff6430a2fb54f84c
Jacob Pledger <jacob@jacobpledger.com>
2019-06-26 12:16:18 -0600

    remaining futurize fixes, added for compatibility sake

--- django_opentracing/tracer.py
+++ django_opentracing/tracer.py
@@ -71,7 +71,7 @@ class DjangoTracer(object):
         '''
         # strip headers for trace info
         headers = {}
-        for k,v in request.META.items():
+        for k,v in list(request.META.items()):
             k = k.lower().replace('_','-')
             if k.startswith('http-'):
                 k = k[5:]
